4103822520696fa1cf86d46ae05bb9e6b86ddbf2
joeoke <joe.okelue7@gmail.com>
2021-03-16 01:00:08 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Handler for Cancel Button
+const cancelFunction=()=>{
+
+// Get the modal
+let modal = document.getElementById("modal-cancel");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
